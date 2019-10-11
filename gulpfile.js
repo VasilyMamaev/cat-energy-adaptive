ff193d293a9d422b5d793c3f553994cca436e6b4
VasilyMamaev <mamaev93@mail.ru>
2019-10-11 21:50:58 +0500

    оформил каталог

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ var sassGlob = require('gulp-sass-glob');
 var browserSync = require('browser-sync');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
+var svgstore = require('gulp-svgstore');
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
  
 gulp.task('styles', function () {
     return gulp
@@ -47,3 +52,36 @@ gulp.task('watch', function() {
     gulp.watch('source/libs/**/*.js', browserSync.reload);
 });
 gulp.task('start', gulp.parallel('styles', 'browser-sync', 'watch'));
+
+gulp.task('compress', function() {
+    return gulp.src('source/img/*')
+    .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('source/img'));
+});
+
+gulp.task('webp', function() {
+    return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest('source/img'));
+});
+
+gulp.task('sprite', function() {
+    return gulp.src('source/img/icon-*.svg')
+        .pipe(svgstore({
+            inlineSvg: true
+    }))
+        .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('source/img'));
+});
+
+gulp.task('html', function() {
+    return gulp.src('source/*.html')
+    .pipe(posthtml([
+        include()
+    ]))
+    .pipe(gulp.dest('source'))
+});
